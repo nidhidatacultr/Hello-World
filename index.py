d346957552e0c29e0de4e12929871154e84f19fa
Nidhi Gautam <95139681+nidhidatacultr@users.noreply.github.com>
2022-02-22 16:34:06 +0530

    Create index.py
    I have added first file.

---
+++ index.py
@@ -0,0 +1,8 @@
+## python Program to swap two numbers
+x = 5
+y = 6
+temp = x
+x = y
+y = temp
+print(f" The value of x after swapping is : {x}\n")
+print(f" The value of y after swapping is : {y}")
